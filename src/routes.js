0227cfb8c07e707294efed1e96f9f56880d31e78
Gaduh Hartawan <gaduhhartaone@gmail.com>
2022-12-22 13:53:43 +0700

    tambah route baru untuk admin

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,11 @@ const TypeList = React.lazy(() => import('./views/pages/admin/TypeList'))
 const PeminjamanList = React.lazy(() => import('./views/pages/admin/PeminjamanList'))
 const PengembalianList = React.lazy(() => import('./views/pages/admin/PengembalianList'))
 const AddUser = React.lazy(() => import('./views/pages/forms/AddUser'))
+const EditUser = React.lazy(() => import('./views/pages/forms/EditUser'))
+const AddAdmin = React.lazy(() => import('./views/pages/forms/AddAdmin'))
+const EditAdmin = React.lazy(() => import('./views/pages/forms/EditAdmin'))
+const AddBook = React.lazy(() => import('./views/pages/forms/AddBook'))
+const EditBook = React.lazy(() => import('./views/pages/forms/EditBook'))
 
 const routes = [
   // { path: '/', exact: true, name: 'Home' },
@@ -25,6 +30,11 @@ const routes = [
   { path: '/admin/pengembalian', name: 'Pengembalian', element: PengembalianList },
   // Admin Forms
   { path: '/admin/adduser', name: 'Form Add User', element: AddUser },
+  { path: '/admin/edituser/:id', name: 'Form Edit User', element: EditUser },
+  { path: '/admin/addadmin', name: 'Form Add Admin', element: AddAdmin },
+  { path: '/admin/editadmin/:id', name: 'Form Edit Admin', element: EditAdmin },
+  { path: '/admin/addbook', name: 'Form Add Book', element: AddBook },
+  { path: '/admin/editbook/:id', name: 'Form Edit Book', element: EditBook },
 
   // User
 ]
